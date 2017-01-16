45439af5c386b9e1ca89adc201ad9e146c449729
Eduardo <zerhiphop@live.com>
2017-01-16 07:53:54 -0500

    Modificacion del archivo manejadorCuenta.java añadiendo codigo de User Entity Manager

--- ModuloBancario/src/java/ModuloBancario/Session/manejadorCuenta.java
+++ ModuloBancario/src/java/ModuloBancario/Session/manejadorCuenta.java
@@ -5,6 +5,8 @@
 package ModuloBancario.Session;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -12,6 +14,12 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class manejadorCuenta implements manejadorCuentaLocal {
+    @PersistenceContext(unitName = "ModuloBancarioPU")//insert code UserEntityManager
+    private EntityManager em;//Instancia que administra todas las entidades que tengo declaradas
+
+    public void persist(Object object) {
+        em.persist(object);//Toma un objeto y se lo pasa al entity manager, el persist instruye en base a los estados al jpa que construya el sql necesario para poder hacer las operaciones en la bdd
+    }
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")

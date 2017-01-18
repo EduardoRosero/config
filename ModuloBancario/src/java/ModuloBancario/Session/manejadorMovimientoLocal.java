97d689482743c213bfebcc3e621bc7fdddeedbc8
Eduardo <zerhiphop@live.com>
2017-01-18 15:49:40 -0500

    Creacion de los archivos manejadorMovimiento.java y manejadorMovimientoLocal.java

---
+++ ModuloBancario/src/java/ModuloBancario/Session/manejadorMovimientoLocal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModuloBancario.Session;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author eduardo
+ */
+@Local
+public interface manejadorMovimientoLocal {
+    
+}
